a300ade9981720cbab94b03fe3f3abeb679aef41
Lakindu Perera <86124152+LAKEY82@users.noreply.github.com>
2022-06-06 19:29:37 +0530

    1st commit

---
+++ src/Pages/home.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import AppsIcon from '@material-ui/icons/Apps';
+import {Avatar} from '@material-ui/core';
+import pic from "../Images/name.jpg"
+import '../Pages/home.css'
+import search  from './search';
+import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
+function home() {
+  return (
+    <div className='home'>
+    <div className='home_header'>
+
+            <div className='home_headerLeft'>
+               <p>About</p>
+               <p>Store</p>
+            </div>
+
+            <div className='home_headerRight'>
+              <p>Gmail</p>
+              <p>Images</p>
+              <AppsIcon/>
+              <Avatar/>
+            </div>
+
+    </div>
+    
+    <div className='home_body'>
+         <img src={pic} />
+         
+          <div className='home_inputcontainer'>
+          <div>   
+             <form className='search'>
+                 <div className='search_input'>
+                 <SearchIcon/> 
+                   <input/>
+                   <MicIcon/> 
+                 </div>
+
+                 <div className='search_buttons'>
+                   <button type="submit" variant="outlined">Galgoo Search</button>
+
+                   <button  variant="outlined">I'm Feeling Lucky</button>
+                 </div>
+             </form>
+          </div> 
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default home
---
+++ src/Pages/search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Pages/search.css';
+function search() {
+  return (
+    <form className='search'>
+    search
+    
+    </form>
+  )
+}
+
+export default search
